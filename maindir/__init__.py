ba411c6619f49fa13410dda715061349e313e37c
june! <thecenterofthree@gmail.com>
2022-06-11 20:24:23 -0400

    added config.py

--- maindir/__init__.py
+++ maindir/__init__.py
@@ -1,10 +1,11 @@
 from flask import Flask
 from .views import views
+from .config import *
 
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'falh sakhfdlsjahfasldfs '
+    app.config['SECRET_KEY'] = secret_key
 
     app.register_blueprint(views, url_prefix='/')
 
